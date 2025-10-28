9868f26f6cdd6dad7e2d04bb462a8ed913eb9c42
angelica1822 <113204229+angelica1822@users.noreply.github.com>
2025-10-28 13:50:03 -0500

    Refactor virtual address handling and input validation

--- lab-7.c
+++ lab-7.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 
 int main(int argc, char **argv)
 {
     if (argc == 2)
     {
         int VA = 32, page_size = 4096, bits = 12;
-        unsigned int vaddr = atoi(argv[1]);
-        unsigned int page_numer=vaddr >> bits;
-        unsigned int offset=vaddr & (page_size-1);
-        printf("VA Size[%d] Bits\n",VA);
-        printf("Page Size es [4KB][%d bits]\n",bits);
-        printf("The address [%u]=[%Xh] contains:\n",vaddr ,vaddr);
-        printf("page number=[%u]=[%Xh]\n",page_numer,page_numer);
-        printf("offset=[%u]=[%Xh]\n",offset,offset);
+        char *endptr;
+
+        unsigned long vaddr = strtoul(argv[1], &endptr, 10);
+
+        if (*endptr != '\0') {
+            printf("Usage: ./program <virtual address (decimal)>\n");
+            return 1;
+        }
+
+        unsigned long page_number = vaddr >> bits;
+        unsigned long offset = vaddr & (page_size - 1);
+
+        printf("VA Size[%d] Bits\n", VA);
+        printf("Page Size is [4KB][%d bits]\n", bits);
+        printf("The address [%lu]=[%lXh] contains:\n", vaddr, vaddr);
+        printf("page number=[%lu]=[%lXh]\n", page_number, page_number);
+        printf("offset=[%lu]=[%lXh]\n", offset, offset);
     }
-    
     else
     {
         printf("Usage: ./program <virtual address (decimal)>\n");
